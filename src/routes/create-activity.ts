42ca7a5df0b43bea3416d941ce8727f5607abbf3
João Fábio Marques <jfmarques2468@gmail.com>
2024-07-13 12:30:37 -0300

    Aula 02 Concluída

--- src/routes/create-activity.ts
+++ src/routes/create-activity.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
 
 export async function createActivity(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -28,8 +29,22 @@ export async function createActivity(app: FastifyInstance) {
       if (!trip) {
         throw new Error("Trip not found.");
       }
+      if (dayjs(occurs_at).isBefore(trip.starts_at)) {
+        throw new Error("Invalid activity date.");
+      }
+      if (dayjs(occurs_at).isAfter(trip.ends_at)) {
+        throw new Error("Invalid activity date.");
+      }
 
-      return { tripId: trip.id };
+      const activity = await prisma.activity.create({
+        data: {
+          title,
+          occurs_at,
+          trip_id: tripId,
+        },
+      });
+
+      return { activityId: activity.id };
     }
   );
 }
